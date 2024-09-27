d23691ad2d674acd899f3136fbb25b27943f3850
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2024-09-27 10:34:40 +0530

    Create MaxProductSubArray.cpp

---
+++ MaxProductSubArray.cpp
@@ -0,0 +1,30 @@
+//152. Maximum Product Subarray
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) 
+    {
+        //KADANES ALGORITHM
+        int maxp = INT_MIN;
+        int prod=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            prod = prod*nums[i];
+            maxp = max(maxp,prod);
+            if(prod==0)
+            {
+                prod=1;
+            }
+        }
+        prod=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            prod = prod*nums[i];
+            maxp = max(maxp,prod);
+            if(prod==0)
+            {
+                prod=1;
+            }
+        }
+        return maxp;
+    }
+};
